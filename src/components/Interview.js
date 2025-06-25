f8ec570a14f671920c309981cd8bc8a34f7bf00f
Hiren Khanchandani <hirenkhanchandani14@gmail.com>
2025-06-24 21:19:21 -0400

    bug fixes

--- src/components/Interview.js
+++ src/components/Interview.js
@@ -344,7 +344,7 @@ const Interview = () => {
             onClick={() => {
                 stopRecording();
                 stopSpeaking();
-                navigate("/");
+                navigate(-1);
             }}
             className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600"
           >
